33e8d7b826877591999c0965dd87bf266b144ee6
mehdi_lp <mohammadmehdirajabi9@gmail.com>
2023-04-10 13:13:34 +0430

    add prime_fibo project

---
+++ prime_fibo/prime_fibo/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace prime_fibo
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.isPrimBtn = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.fiboBtn = new System.Windows.Forms.Button();
+			this.numberTxt = new System.Windows.Forms.TextBox();
+			this.resultTxt = new System.Windows.Forms.TextBox();
+			this.clearBtn = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// isPrimBtn
+			// 
+			this.isPrimBtn.Location = new System.Drawing.Point(159, 89);
+			this.isPrimBtn.Name = "isPrimBtn";
+			this.isPrimBtn.Size = new System.Drawing.Size(75, 23);
+			this.isPrimBtn.TabIndex = 2;
+			this.isPrimBtn.Text = "Is Prime?";
+			this.isPrimBtn.UseVisualStyleBackColor = true;
+			this.isPrimBtn.Click += new System.EventHandler(this.isPrimBtn_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(23, 48);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(116, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "enter your number here";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(266, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(32, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "result";
+			// 
+			// fiboBtn
+			// 
+			this.fiboBtn.Location = new System.Drawing.Point(159, 134);
+			this.fiboBtn.Name = "fiboBtn";
+			this.fiboBtn.Size = new System.Drawing.Size(75, 23);
+			this.fiboBtn.TabIndex = 5;
+			this.fiboBtn.Text = "Fibonacci";
+			this.fiboBtn.UseVisualStyleBackColor = true;
+			this.fiboBtn.Click += new System.EventHandler(this.fiboBtn_Click);
+			// 
+			// numberTxt
+			// 
+			this.numberTxt.Location = new System.Drawing.Point(145, 45);
+			this.numberTxt.Name = "numberTxt";
+			this.numberTxt.Size = new System.Drawing.Size(100, 20);
+			this.numberTxt.TabIndex = 6;
+			// 
+			// resultTxt
+			// 
+			this.resultTxt.Location = new System.Drawing.Point(304, 45);
+			this.resultTxt.Name = "resultTxt";
+			this.resultTxt.Size = new System.Drawing.Size(100, 20);
+			this.resultTxt.TabIndex = 7;
+			// 
+			// clearBtn
+			// 
+			this.clearBtn.Location = new System.Drawing.Point(313, 89);
+			this.clearBtn.Name = "clearBtn";
+			this.clearBtn.Size = new System.Drawing.Size(75, 23);
+			this.clearBtn.TabIndex = 8;
+			this.clearBtn.Text = "Clear";
+			this.clearBtn.UseVisualStyleBackColor = true;
+			this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(439, 197);
+			this.Controls.Add(this.clearBtn);
+			this.Controls.Add(this.resultTxt);
+			this.Controls.Add(this.numberTxt);
+			this.Controls.Add(this.fiboBtn);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.isPrimBtn);
+			this.Name = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button isPrimBtn;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button fiboBtn;
+		private System.Windows.Forms.TextBox numberTxt;
+		private System.Windows.Forms.TextBox resultTxt;
+		private System.Windows.Forms.Button clearBtn;
+	}
+}
+
---
+++ prime_fibo/prime_fibo/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prime_fibo
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			resultTxt.Enabled = false;
+		}
+
+		private void isPrimBtn_Click(object sender, EventArgs e)
+		{
+			int num;
+			if (Int32.TryParse(numberTxt.Text, out num))
+			{
+				resultTxt.Text = IsPrime(num);
+			}
+			else
+			{
+				resultTxt.Text = "invalid number";
+			}
+		}
+
+		static String IsPrime(int num)
+		{
+			if(num == 1 || num == 2)
+				return "prime";
+			for (int i = 2; i < Math.Sqrt(num); i++)
+				if (num % i == 0)
+					return "not prime";
+			return "prim";
+		}
+
+		static int fibo(int num)
+		{
+			if (num == 0) return 0;
+
+			int now = 0;
+			int next = 1;
+			for (int i = 1; i < num; i++)
+			{
+				int sum = now + next;
+				now = next;
+				next = sum;
+			}
+			return next;
+		}
+
+		private void fiboBtn_Click(object sender, EventArgs e)
+		{
+			int num;
+			if (Int32.TryParse(numberTxt.Text, out num))
+			{
+				resultTxt.Text = Convert.ToString(fibo(num));
+			}
+			else
+			{
+				resultTxt.Text = "invalid number";
+			}
+		}
+
+		private void clearBtn_Click(object sender, EventArgs e)
+		{
+			resultTxt.Text = "";
+			//resultTxt.Clear();
+		}
+	}
+}
